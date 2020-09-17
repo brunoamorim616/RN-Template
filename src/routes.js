a53b25dc356050fad29777caf972040b9ebbae54
Desenvolvimento <desenvolvimento@Mac-mini-de-Desenvolvimento.local>
2020-09-17 13:41:42 -0300

    template funcionando MacOS

---
+++ src/routes.js
@@ -0,0 +1,24 @@
+
+import * as React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
+
+import Home from './views/Home';
+
+const Stack = createStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+    return (
+      <NavigationContainer>
+        <MyStack/>
+      </NavigationContainer>
+    );
+  }
