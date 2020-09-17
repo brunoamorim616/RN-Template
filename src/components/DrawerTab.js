b0cf6f29dc97b64d6ce115a7ca721edf69f1156e
Desenvolvimento <desenvolvimento@Mac-mini-de-Desenvolvimento.local>
2020-09-17 18:38:03 -0300

    Zzz

---
+++ src/components/DrawerTab.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View} from 'react-native';
+
+// import { Container } from './styles';
+
+const DrawerTab = () => {
+  return <View />;
+};
+
+export default DrawerTab;
